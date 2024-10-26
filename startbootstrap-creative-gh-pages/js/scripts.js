ea0c1aeeeaf3e01531f1db1655d0d4e5bd1bc400
emili98 <emili.arismendi@gmail.com>
2024-10-26 18:06:47 -0500

    mision 2

---
+++ startbootstrap-creative-gh-pages/js/scripts.js
@@ -0,0 +1,85 @@
+/*!
+* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+let listaUsuarios = []
+function verify() {
+    listaUsuarios = JSON.parse(localStorage.getItem("usuario"))
+}
+
+
+function registrarse() {
+    let nombre = document.getElementById("exampleFirstName").value
+    let apellido = document.getElementById("exampleLastName").value
+    let email = document.getElementById("exampleInputEmail").value
+    let password = document.getElementById("exampleInputPassword").value
+    let repassword = document.getElementById("exampleRepeatPassword").value
+
+
+    console.log(nombre)
+    if (nombre == "") (
+        alert("debe ingresar un nombre")
+    )
+
+    if (apellido == "") (
+        alert("debe ingresar un apellido")
+    )
+
+    if (email == "") (
+        alert("debe ingresar un email")
+    )
+
+    if (password == "") (
+        alert("debe ingresar un password")
+    )
+
+    if (password != repassword) (
+        alert("las contraseñas deben ser iguales")
+    )
+
+    let persona = {
+        nombre: nombre,
+        apellido: apellido,
+        email: email,
+        password: repassword,
+    }
+
+    let usuario = JSON.stringify(persona)
+
+    listaUsuarios.push(usuario);
+
+    localStorage.setItem("usuario", JSON.stringify(listaUsuarios));
+    alert("usuario registrado")
+
+}
+
+
+function ingresar() {
+    //console.log(listaUsuarios)
+}
+
+function verificarlogin() {
+    let listaUsuarios = JSON.parse(localStorage.getItem("usuario"))
+    let email = document.getElementById("exampleInputEmail").evalue
+    let password = document.getElementById("exampleInputPassword").evalue
+
+    for (let i = 0; i < listaUsuarios.length; i++) {
+        //consule.log(listaUsuarios[i].nombre)
+        let usuario = JSON.parse(listaUsuarios[i])
+        console.log(usuario.email)
+        if (email == usuario.email && usuario.password) {
+            alert(" el usuario ingreso correctamente")
+        }
+    }
+
+    if (legado) {
+        window.location.href = "index.html"
+    } else {
+        alert("el usuario o clave incorrecta")
+          }
+}

8199e31016e0d744648cd8ed3d866b741ae1a63b
luistheneonwizardhome <luisjofaba@gmail.com>
2024-10-28 09:34:28 -0500

    28

--- startbootstrap-creative-gh-pages/js/scripts.js
+++ startbootstrap-creative-gh-pages/js/scripts.js
@@ -1,12 +1,3 @@
-/*!
-* Start Bootstrap - Creative v7.0.7 (https://startbootstrap.com/theme/creative)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
-*/
-//
-// Scripts
-// 
-
 let listaUsuarios = []
 function verify() {
     listaUsuarios = JSON.parse(localStorage.getItem("usuario"))
